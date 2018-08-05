4703ec27de0a4f69919ba51977f90bf5fe2e2ba2
Erick Zhao <erick.zhao@mail.mcgill.ca>
2018-08-04 23:08:26 -0400

    Fill in core

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const store = {
-  state: {},
-  listeners: [],
-  dispatch: () => {},
-  subscribe: () => {},
-  getState: () => {}
+const createStore = (reducer, initialState = {}) => {
+  const store = {};
+  
+  // Current state of the store
+  store.state = initialState;
+
+  // Listeners are functions that execute when an action is dispatched
+  store.listeners = [];
+
+  // Following a UI interaction, the component calls the dispatch function
+  // to modify the state and execute each listener function
+  store.dispatch = (action) => {
+    store.state = reducer(store.state, action);
+    store.listeners.forEach(listener => listener());
+  };
+
+  store.subscribe = (listener) => {
+    store.listeners.push(listener);
+  };
+
+  // Exposes the state
+  store.getState = () => store.state;
+
+  return store;
 }
